a81c4cb8ba1f6c8192febc48220a135d84a27339
abodi-Massarweh <abodi.msarwi@gmail.com>
2020-04-06 19:41:21 +0300

    modified .gitignore

---
+++ FamilyTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by abodi on 06/04/2020.
+//
+
+#include "FamilyTree.hpp"
---
+++ FamilyTree.hpp
@@ -0,0 +1,14 @@
+//
+// Created by abodi on 06/04/2020.
+//
+
+#ifndef FAMILYTREE_A_FAMILYTREE_HPP
+#define FAMILYTREE_A_FAMILYTREE_HPP
+
+
+class FamilyTree {
+
+};
+
+
+#endif //FAMILYTREE_A_FAMILYTREE_HPP
